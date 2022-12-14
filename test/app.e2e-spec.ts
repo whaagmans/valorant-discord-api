b9934a7d1c7c45056aa83371e067443a94be9a54
Redeamer <wehaagmans@gmail.com>
2022-12-14 20:47:46 +0100

    chore(#10): :fire: Remove package-lock.json and fix yarn.lock

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,5 @@
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
