13f91ae1d48055516e63ba5a307650f523ff5d9a
Redeamer <wehaagmans@gmail.com>
2022-12-14 20:31:19 +0100

    feat(#10): :package: Add Nestjs resource bot for api calls

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BotModule } from './bot/bot.module';
 
 @Module({
-	imports: [ConfigModule.forRoot()],
+	imports: [ConfigModule.forRoot(), BotModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
