39e4ce3e6ea7e9c0c3791203459086db946be545
Redeamer <wehaagmans@gmail.com>
2022-12-14 20:37:09 +0100

    test(#10): :white_check_mark: Tests emptied for now

--- src/bot/bot.controller.spec.ts
+++ src/bot/bot.controller.spec.ts
@@ -3,18 +3,18 @@ import { BotController } from './bot.controller';
 import { BotService } from './bot.service';
 
 describe('BotController', () => {
-  let controller: BotController;
+	let controller: BotController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [BotController],
-      providers: [BotService],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			controllers: [BotController],
+			providers: [BotService],
+		}).compile();
 
-    controller = module.get<BotController>(BotController);
-  });
+		controller = module.get<BotController>(BotController);
+	});
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(controller).toBeDefined();
+	});
 });
