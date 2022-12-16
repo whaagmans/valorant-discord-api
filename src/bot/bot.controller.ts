87cae99ba95f90c4e29203884900b067038e4e7e
Redeamer <wehaagmans@gmail.com>
2022-12-16 03:48:48 +0100

    feat(#10): :construction: Step 1 of auth process working

--- src/bot/bot.controller.ts
+++ src/bot/bot.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import axios from 'axios';
+import { wrapper } from 'axios-cookiejar-support';
+import { CookieJar } from 'tough-cookie';
 import { BotService } from './bot.service';
 
 @Controller('bot')
 export class BotController {
-	constructor(private readonly botService: BotService) {}
+	client: any;
+	constructor(private readonly botService: BotService) {
+		const jar = new CookieJar();
+		const _client = wrapper(axios.create({ jar }));
+		this.client = _client;
+	}
+
+	@Post()
+	async loginUser(): Promise<string> {
+		return await this.botService.loginRiot(this.client);
+	}
 }
--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 
 @Injectable()
-export class BotService {}
+export class BotService {
+	async loginRiot(client): Promise<any> {
+		const data = {
+			client_id: 'play-valorant-web-prod',
+			nonce: 1,
+			redirect_uri: 'https://playvalorant.com/opt_in',
+			response_type: 'token id_token',
+			scope: 'account openid',
+		};
+		const api_url = 'https://auth.riotgames.com/api/v1/authorization';
+		try {
+			const response = await client
+				.post(
+					api_url,
+					{
+						client_id: 'play-valorant-web-prod',
+						nonce: 1,
+						redirect_uri: 'https://playvalorant.com/opt_in',
+						response_type: 'token id_token',
+						scope: 'account openid',
+					},
+					{
+						headers: {
+							'User-Agent': 'PostmanRuntime/7.30.0',
+						},
+					}
+				)
+				.then((data) => console.log(data.status))
+				.catch((err) => {
+					throw new InternalServerErrorException(err);
+				});
+		} catch (err) {
+			throw new InternalServerErrorException(err);
+		}
+	}
+}
