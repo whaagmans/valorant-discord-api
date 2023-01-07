57deace8fa37f1f4f5ec6164da8c161d62cd0e98
Redeamer <wehaagmans@gmail.com>
2023-01-07 18:18:28 +0100

    feat(#10): :sparkles: Feature riot sign in process complete

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -1,10 +1,14 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import axios from 'axios';
 import { CookieJar } from 'tough-cookie';
+import { UserMfaDTO } from './DTO/UserMfa.dto';
+import { UserSignInDTO } from './DTO/UserSignIn.dto';
 
 @Injectable()
 export class BotService {
-	async loginRiot(cookieJar: CookieJar): Promise<any> {
+	private readonly api_url = 'https://auth.riotgames.com/api/v1/authorization';
+
+	async getRiotSessionCookie(cookieJar: CookieJar): Promise<any> {
 		const data = {
 			client_id: 'play-valorant-web-prod',
 			nonce: 1,
@@ -12,10 +16,56 @@ export class BotService {
 			response_type: 'token id_token',
 			scope: 'account openid',
 		};
-		const api_url = 'https://auth.riotgames.com/api/v1/authorization';
 
 		const response = await axios
-			.post(api_url, data, {
+			.post(this.api_url, data, {
+				headers: {
+					'User-Agent': 'PostmanRuntime/7.30.0',
+				},
+				jar: cookieJar,
+				withCredentials: true,
+			})
+			.catch((err) => {
+				throw new InternalServerErrorException(err);
+			});
+		console.log(response);
+
+		return await cookieJar;
+	}
+
+	async riotUserLogin(user: UserSignInDTO, cookieJar: CookieJar): Promise<any> {
+		const data = {
+			type: user.type,
+			username: user.username,
+			password: user.password,
+			remember: user.remember,
+		};
+		const response = await axios
+			.put(this.api_url, data, {
+				headers: {
+					'User-Agent': 'PostmanRuntime/7.30.0',
+				},
+				jar: cookieJar,
+				withCredentials: true,
+			})
+			.catch((err) => {
+				throw new InternalServerErrorException(err);
+			});
+		console.log(response);
+		console.log(cookieJar);
+
+		return await response.data;
+	}
+
+	async mfaSignIn(user: UserMfaDTO, cookieJar: CookieJar): Promise<any> {
+		const data = {
+			type: user.type,
+			rememberDevice: user.rememberDevice,
+			code: user.code,
+		};
+
+		const response = await axios
+			.put(this.api_url, data, {
 				headers: {
 					'User-Agent': 'PostmanRuntime/7.30.0',
 				},
