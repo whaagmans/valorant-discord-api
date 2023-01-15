d57ec90e114ca60cee0d165f788c11a5a3bd9300
Redeamer <wehaagmans@gmail.com>
2023-01-15 21:36:58 +0100

    feat(#10): :label: Update type to replace removed file

--- src/user/DTO/User.dto.ts
+++ src/user/DTO/User.dto.ts
@@ -1,8 +1,8 @@
-import { RiotCookies } from './RiotCookies.dto';
-
 export class User {
 	discord_id: string;
 	api_key: string;
+	valorant_id: string;
 	valorant_username: string;
-	RiotCookies: RiotCookies;
+	ssid: string;
+	tdid: string;
 }
