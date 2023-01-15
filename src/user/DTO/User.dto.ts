918503bcc718eeb2e8dcacf32bfa2b11fa82a782
Redeamer <wehaagmans@gmail.com>
2023-01-15 17:34:18 +0100

    feat(#10): :label: Forgot to add type to model value.

--- src/user/DTO/User.dto.ts
+++ src/user/DTO/User.dto.ts
@@ -3,6 +3,6 @@ import { RiotCookies } from './RiotCookies.dto';
 export class User {
 	discord_id: string;
 	api_key: string;
-	valorant_username;
+	valorant_username: string;
 	RiotCookies: RiotCookies;
 }
